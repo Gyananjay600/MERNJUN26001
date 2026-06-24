77f93659e0065bf455aab28dc2d8fa32543a2b21
GYANANJAY SAHOO <gyananjaysahoo84@gmail.com>
2026-06-24 23:05:23 +0530

    javascrip operator and conditional statement completed

---
+++ Month-1/Week-3/Day-3/Practice/script.js
@@ -0,0 +1,30 @@
+console.log("Hello World");
+
+// compairison operators
+// ==, ===, !=, !==, >, <, >=, <=
+
+console.log(5 == 5); // true
+console.log(5 === 5);
+
+console.log(5 != 5);
+console.log(5 !== 5);
+
+console.log(5 > 9);
+console.log(5 < 9);
+
+console.log(5 >= 5);
+console.log(5 <= 5);
+
+console.log("hii" == 3);
+console.log("2" == 2);
+
+// strict checking 
+console.log("2" === 2);
+
+// type casting
+console.log(+"2" === 2);
+
+console.log({} == {}); // false
+
+console.log([] == []);
+
