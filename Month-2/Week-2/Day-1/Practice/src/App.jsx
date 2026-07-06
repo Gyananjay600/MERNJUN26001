2a25a192afc85439202f6ef67b19e0204eda1411
GYANANJAY SAHOO <gyananjaysahoo84@gmail.com>
2026-07-06 13:12:00 +0530

    react folder structure

---
+++ Month-2/Week-2/Day-1/Practice/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    
+    <div>
+      HII
+      <Navbar />
+<h1>Welcome to My App</h1>
+
+    </div>
+  )
+}
+
+export default App
---
+++ Month-2/Week-2/Day-1/Practice/src/Navbar.jsx
@@ -0,0 +1,11 @@
+// rfce : This is a shortcut to create a React functional component with export
+
+function Navbar() {
+  return (
+    <div>
+      <h1>My App</h1>
+    </div>
+  )
+}
+
+export default Navbar
