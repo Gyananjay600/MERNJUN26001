fcfd0c483609b0de40c99242c6bb2c51c44c9f1e
GYANANJAY SAHOO <gyananjaysahoo84@gmail.com>
2026-07-08 21:42:19 +0530

    hooks concept handling concept

---
+++ Month-2/Week-2/Day-3/Practice/src/Home.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from "react";
+import { nameContext, themeContext } from "./App";
+
+
+function Home() {
+  const name = useContext(nameContext);
+  const theme=useContext(themeContext)
+  return (
+    <div>
+      Home
+      <br />
+      Name : {name}
+
+      <br />
+      theme :{theme}
+
+      
+    </div>
+  );
+}
+
+export default Home;
